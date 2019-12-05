38e205e6c52ec55927e541c3738762bb38518985
Darctan <v.p.lehtola@gmail.com>
2019-12-05 06:46:21 +0200

    Day 4 solutions

--- src/Solutions2019/Solutions.hs
+++ src/Solutions2019/Solutions.hs
@@ -1,17 +1,13 @@
 module Solutions2019.Solutions where
 
-import Control.Monad
-import Data.String  
-import Debug.Trace  
 import Data.List.Split
-import Data.Function
-
-import Advent
+import Data.List
 
 solutions :: [(Integer, (String -> String, String -> String))]
 solutions = [ (1, (day1part1, day1part2)),
   (2, (day2part1, day2part2)),
-  (3, (day3part1, day3part2))]
+  (3, (day3part1, day3part2)),
+  (4, (day4part1, day4part2))]
 
 day1part1 :: String -> String
 day1part1 = show . sum . map (fuelneed . (read :: String -> Int)) . lines
@@ -42,20 +38,21 @@ run program counter =
         newMemory f = modify program i (f (program !! x) (program !! y))
 
 day2part1 :: String -> String
-day2part1 = show . (!!0) . (`run` 0) . (\(x:y:z:rest) -> x:12:2:rest) . map read . splitOn ","
+day2part1 = show . (!!0) . (`run` 0) . (\(x:_:_:rest) -> x:12:2:rest) . map read . splitOn ","
 
 pairs :: [(Value, Value)]
 pairs = [(x, y) | x <- [0 .. 99], y <- [0 .. 99]]
 
 day2part2 :: String -> String
 day2part2 = show . (\(x, y) -> x * 100 + y) . fst . head . filter (\(inp, prg) -> ((==19690720) . head . (`run` 0) . input inp) prg) . zip pairs . repeat . map (read :: String -> Value) . splitOn ","
-  where input (i, j) (x:y:z:rest) = x:i:j:rest
+  where input (i, j) (x:_:_:rest) = x:i:j:rest
+        input _ _ = undefined
 
 data Direction = DRight | DLeft | DUp | DDown
 
 path :: (Int, Int) -> [(Direction, Int)] -> [(Int, Int)]
 path _ [] = []
-path pos ((dir, 0):rest) = path pos rest
+path pos ((_, 0):rest) = path pos rest
 path pos ((dir, n):rest) = new : path new ((dir, n - 1):rest)
   where (x, y) = pos
         new = case dir of
@@ -87,6 +84,30 @@ snds (x, y) = (snd x, snd y)
 
 day3part2 :: String -> String
 day3part2 = show . minimum . map (uncurry (+) . fsts) . filter (uncurry (==) . snds) . (\[xs, ys] -> [(x, y) | x <- xs, y <- ys]) . map (zip [1..] . path (0, 0) . map readDirection . splitOn ",") . lines
+
+checkNumber :: Int -> Bool -> Int -> Bool
+checkNumber _ hasDouble 0 = hasDouble
+checkNumber prevDigit hasDouble number = prevDigit >= r && checkNumber r (hasDouble || prevDigit == r) q
+  where (q, r) = number `quotRem` 10
+
+day4part1 :: String -> String
+day4part1 = show . length . filter (checkNumber 10 False) . (\[x, y] -> [x .. y]) . map read . splitOn "-"
+
+toDigits :: Int -> [Int]
+toDigits = reverse . toDigits'
+
+toDigits' :: Int -> [Int]
+toDigits' 0 = []
+toDigits' number = r : toDigits' q
+  where (q, r) = number `quotRem` 10
+
+ascending :: (Ord a) => [a] -> Bool
+ascending [] = True
+ascending [_] = True
+ascending (x:y:xs) = x <= y && ascending (y:xs)
+  
+day4part2 :: String -> String
+day4part2 = show . length . filter (any $ (==2) . length) . map group . filter ascending . map toDigits . (\[x, y] -> [x .. y]) . map read . splitOn "-"
 
 dayxparty :: String -> String
 dayxparty = undefined
